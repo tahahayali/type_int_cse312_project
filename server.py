81f738598e4d62cbd0719b39d5059f64f70f5d0e
Ahmed <ahmedbouhraoua0105@gmail.com>
2025-04-28 01:40:03 -0400

    fixing avatar page routes

--- server.py
+++ server.py
@@ -56,6 +56,10 @@ def game():
 def home():
     return send_from_directory('public/html', 'home_page.html')
 
+@app.route('/upload_avatar')
+def home():
+    return send_from_directory('public/html', 'upload_avatar.html')
+
 @app.post('/upload_avatar')
 def avatar_upload_route():
     return upload_avatar()
